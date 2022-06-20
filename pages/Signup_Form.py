e85468c49a530c19af91d68f9e76c94c3efa4c4d
HamidBekamiri <hamid.bekam@gmail.com>
2022-06-20 12:44:03 +0200

    connecting googlesheet dataset

--- pages/Signup_Form.py
+++ pages/Signup_Form.py
@@ -6,6 +6,8 @@ import streamlit as st
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 from folium.plugins import Draw
 from streamlit_folium import st_folium
+import pygsheets
+import pandas as pd
 
 def intro():
     st.write("# Welcome to Danish AI Mapping! 👋")
@@ -17,36 +19,54 @@ Danish AI Mapping is an app built specifically for landscaping AI companies in D
         """
     )
 
+st.write("# Welcome to Danish AI Mapping!")
 
-def signup_form():
-    st.write("# Welcome to Danish AI Mapping!")
-
-    st.markdown(
-        """
+st.markdown(
+    """
 Danish AI Mapping is an app built specifically for landscaping AI companies in Denmark.
 
-        """
-    )
-    df_template = pd.DataFrame(
-    '',
-    index=range(10),
-    columns=list('abcde'))
-    st.subheader("Create New Profile for Your Company")
-    startup_name = st.text_input("Company Name")
-    startup_url = st.text_input("Company URL")
-    startup_founding_year = st.text_input("Company Founding Year")
-    city_state = st.text_input("City")
-    address_street = st.text_input("Street")
-    address_code = st.text_input("City Postal Code")
-    startup_industry = st.text_input("Company Industry")
+    """
+)
 
+def singup():
+    df_template = pd.DataFrame()
+    st.subheader("Create New Profile for Your Company")
+    df_template['startup_name'] = [st.text_input("Company Name")]
+    df_template['startup_url'] = [st.text_input("Company URL")]
+    df_template['startup_founding_year'] = [st.text_input("Company Founding Year")]
+    df_template['city_state'] = [st.text_input("City")]
+    df_template['address_street'] = [st.text_input("Street")]
+    df_template['address_code'] = [st.text_input("City Postal Code")]
+    df_template['startup_industry'] = [st.text_input("Company Industry")]
+    
 
     if st.button("Signup"):
         st.success("You have successfully created a Profile")
-  
+        #authorization
+        SAMPLE_SPREADSHEET_ID = '1jzoF2XLAGtY2yGgjp0mPSlta6KSVg6omHO8ScOOaRSU'
+        gc = pygsheets.authorize(service_file='/Users/zhyarsozhyn/Documents/AIGrowthLab/streamlit_ai_mapping/streamlit_aggrid/keys.json')
+        sht1 = gc.open_by_key(SAMPLE_SPREADSHEET_ID)
+        #select the first sheet 
+        wks = sht1[0]
+        # df_c = wks.get_as_df(has_header=True)
+        # s = pd.Series(df_c['startup_name'].to_list())
+        # st.write(df_c)
+        # b = df_template['startup_name']
+        # st.write(type(b))
+        # st.write(b)
+        # st.write(type(s))
+        # st.write(s)
+
+        # if b in s:
+        #     return st.success("This profile has already been created!")
+        # else:     
+        values = df_template.values.tolist()
+        return wks.append_table(values, start='A1', end=None, dimension='ROWS', overwrite=False)
+       
 
 def reading_data():
     df= pd.read_csv('AIStartupLandscape-DK.csv', index_col=0)
     return df
 
-signup_form()
+singup()
+
