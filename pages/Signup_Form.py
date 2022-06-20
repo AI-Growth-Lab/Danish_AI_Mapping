ff751e76e00d2684d5d0e4a721390cde2497ab2e
HamidBekamiri <hamid.bekam@gmail.com>
2022-06-20 13:33:48 +0200

    requirement

--- pages/Signup_Form.py
+++ pages/Signup_Form.py
@@ -44,7 +44,7 @@ def singup():
         st.success("You have successfully created a Profile")
         #authorization
         SAMPLE_SPREADSHEET_ID = '1jzoF2XLAGtY2yGgjp0mPSlta6KSVg6omHO8ScOOaRSU'
-        gc = pygsheets.authorize(service_file='/Users/zhyarsozhyn/Documents/AIGrowthLab/streamlit_ai_mapping/streamlit_aggrid/keys.json')
+        gc = pygsheets.authorize(service_file='keys.json')
         sht1 = gc.open_by_key(SAMPLE_SPREADSHEET_ID)
         #select the first sheet 
         wks = sht1[0]
